52b7dbe6e99a7e21f3579863e40a696399662309
Xiaoxia Che <chexiaoxia@gmail.com>
2021-08-15 23:32:53 -0400

    Pointer to array

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+// Pointer p1 points to arr[5], it is same with p = arr;
+// Pointer pi points to arr, so if somethings changes array in function print, the arr in main will change at the same time.
+void print(int* p1)
+{
+     for(int i = 0; i < 5; i++)
+    {
+        p1[i] += 1;    
+        cout << p1[i] << " " ;
+    }    
+    cout << endl;
+}
+
+int main()
+{
+    int arr[5] = {1,2,3,4,5};
+    int* p;
+    // The following is same with the function print.
+    p = arr;
+    for(int i = 0; i < 5; i++)
+    {
+        cout << p[i] << " ";
+    }
+    cout << endl;
+
+    print(arr);
+
+    // Pointer pi points to arr, so if somethings changes array in function print, the arr in main will change at the same time.
+    for(int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }    
+    cout << endl;
+    return 0;
+}
